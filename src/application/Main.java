fa0e182306c58b2ffbc836a174165d0e63e5aa98
tobiasvinther <tobi626i@stud.kea.dk>
2021-04-08 13:17:41 +0200

    Forgot to include my answer about inheritance. I have put it in the Main class.

--- src/application/Main.java
+++ src/application/Main.java
@@ -4,5 +4,17 @@ public class Main {
 
     public static void main(String[] args) {
 
+        /*
+        Inheritance is when a class inherits functionality from another class. The top class in this structure is
+        called the super class, while the classes which inherit are called subclasses or child classes.
+        The benefit of inheritance is to hoard generational wealth and thereby perpetuate the unjust class structure
+        in society. Rise up, comrades! Ahem... I mean: the benefit of inheritance is that several classes can share
+        common functionality and thereby reduce the need for copy-pasting the same code several places, which means
+        that the code will be easier to maintain. Subclasses can use and extend the functionality of its super class.
+        Using interfaces, we can also create a “contract” with ourselves, that the classes implementing the
+        interface(s) must implement certain functionality.
+         */
+
+
     }
 }
